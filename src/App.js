a02ff3efc8ce937cd8d151b8b9ca6b6806a14b0d
Jamie <jamie.jansen@live.com>
2018-10-16 15:31:16 +0200

    fixed json, added calc function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 //IMPORT COMPONENTS INTO APP
-import React,  { Component } from 'react'
-import './App.css'
-import FormView from './form-view'
-import data from './data.json'
+import React, { Component } from "react";
+import "./App.css";
+import FormView from "./form-view";
+import data from "./data.json";
 
 //RENDER MESSAGEVIEW TO APP
 class App extends Component {
-    render() {
-        var n = 0
-        return (
-            <div className={'Form'}>
-                {data.map(obj => <FormView key={n += 1}props={obj} />)}
-            </div>
-        )
-    }
+  render() {
+    var number = 0;
+    return (
+      <div className={"Form"}>
+        {data.map(obj => (
+          <FormView key={(number += 1)} props={obj} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/form-view.js
+++ src/form-view.js
@@ -1,20 +1,34 @@
 import React, { Component } from "react"
 
 class FormView extends Component {
-  render() {
+
+    render() {
+    let calc = function() {
+        let answerValue = document.querySelectorAll('input:checked')
+        for(let i = 0; i < answerValue.length; i++) {
+            console.log(answerValue[i].value, i)
+        }
+    }
+    
     const data = this.props
-    console.log(data.props)
+    // console.log(data.props)
     return (
       <div>
+          <div>
+              <h1>{
+
+              }</h1>
+          </div>
         <h1>{data.props.target}</h1>
         <ul>
           {data.props.questions.map(obj => (
-            <li key={obj.id}>
+            <li key={obj.id} className="question-container">
               <h2>{obj.content}</h2>
-              <ul className="q-container">
+              <ul className="answer-container">
                 {obj.answers.map(q => (
                   <li key={q.id} className="selections">
                     <input
+                    onChange={calc}
                       name={obj.content}
                       id={q.id}
                       type={"radio"}
