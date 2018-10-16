ab1a4e6b813e596a38f470c7bebf09c528b7f713
Jamie <jamie.jansen@live.com>
2018-10-16 14:43:24 +0200

    added css styling, fixed JSON

--- src/form-view.js
+++ src/form-view.js
@@ -1,34 +1,35 @@
-import React, {Component} from 'react'
+import React, { Component } from "react"
 
 class FormView extends Component {
-    render() {
-        const data = this.props
-        console.log(data.props)
-        return (
-            <div>
-                <h1>{data.props.target}</h1>
-                <ul>
-                    {data.props.questions.map(obj => 
-                        <li key={obj.id}>
-                            <h2>
-                                {obj.content}
-                            </h2>
-                            <ul>
-                                {obj.answers.map(q =>
-                                    <li key={q.id}>
-                                        <label htmlFor={q.id}>
-                                            {q.content}
-                                        </label>
-                                        <input name={obj.content} id={q.id} type={'radio'} value={q.value} />
-                                    </li>    
-                                )}
-                            </ul>
-                        </li>    
-                    )}
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    const data = this.props
+    console.log(data.props)
+    return (
+      <div>
+        <h1>{data.props.target}</h1>
+        <ul>
+          {data.props.questions.map(obj => (
+            <li key={obj.id}>
+              <h2>{obj.content}</h2>
+              <ul className="q-container">
+                {obj.answers.map(q => (
+                  <li key={q.id} className="selections">
+                    <input
+                      name={obj.content}
+                      id={q.id}
+                      type={"radio"}
+                      value={q.value}
+                    />
+                    <label htmlFor={q.id}>{q.content}</label>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default FormView
